59a8df2d8801292515ec9352e584c6e82eba4cb3
sid114118 <87190484+sid114118@users.noreply.github.com>
2026-03-29 10:09:05 +0530

    Update ProductFeed.jsx

--- src/ProductFeed.jsx
+++ src/ProductFeed.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-// 👇 Notice these exact lowercase filenames with .jsx!
-import ShopFeed from './shop.jsx';   
-import GuestFeed from './guest.jsx'; 
+import ShopFeed from './ProductFeed/ShopFeed.jsx';   
+import GuestFeed from './ProductFeed/GuestFeed.jsx'; 
 
 export default function ProductFeed(props) {
-  // If the user is logged in AND has a primary shop selected
   const hasShop = props.user && props.user.primaryShop;
 
   if (hasShop) {
     return <ShopFeed {...props} />;
   }
 
-  // Otherwise, route them to the Guest Feed
   return <GuestFeed {...props} />;
 }
